56d6e59c4ed9ea0f5ad9f2ba1106d568adea88f5
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-07-30 14:19:28 -0500

    completed for_loops exercises

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,1 +1,38 @@
 "use strict"
+
+function showMultiplicationTable(x) {
+    for (var y = 1; y <= 10; y++) {
+        console.log(x + " X " + y + " = "  + (x*y));
+    }
+}
+
+showMultiplicationTable(9);
+
+console.log("");
+
+function randomNumber(smallestNumber,largestNumber) { 
+    return Math.floor(Math.random() * (largestNumber - smallestNumber) + smallestNumber); }
+
+for (var x = 1; x <= 10 ; x ++){
+    var randomGen = randomNumber(20,200)
+    if (randomGen % 2 === 0){
+        console.log(randomGen + " is even!")
+
+    } else{
+        console.log(randomGen + " is odd!")
+    }
+}
+
+console.log("");
+
+
+for (var pyramid =1; pyramid <= 9; pyramid++){
+    console.log(pyramid.toString().repeat(pyramid))
+}
+ console.log("");
+
+for (var countdown = 100; countdown >= 5; countdown -=5){
+    console.log(countdown);
+}
+
+
