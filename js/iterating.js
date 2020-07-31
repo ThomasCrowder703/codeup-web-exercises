c65995a59573c1dd898ac1661167046fa751d2c6
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-07-30 22:55:04 -0500

    worked on iteratig arrays exercise

--- js/iterating.js
+++ js/iterating.js
@@ -6,24 +6,32 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+var names = ["Thomas","Kobe","Ahmed","Phil"];
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+console.log(names.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
 
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
+
+    for (var i = 0; i < names.length;i++){
+        console.log("The name at " + i + " is " + names[i]);
+    }
 
     /**
      * TODO:
