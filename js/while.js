14303e3d3b68a3eedffe9ddbb06531cc18cb42e7
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-07-31 13:39:39 -0500

    completed while exercises

--- js/while.js
+++ js/while.js
@@ -5,10 +5,18 @@ while(addPrevious <= 65536){
     console.log(addPrevious);
     addPrevious+= addPrevious;
 }
+var allCones = Math.floor(Math.random() * 50) + 50;
+do {
+    var cones = Math.floor(Math.random() * 5) + 1;
+    // decide if we have enough
+    if (cones <= allCones) {
+        //sell some cones
+        console.log(cones + " cones sold...");
+        allCones -= cones;
+    } else {
+        console.log("Cannot sell you " + cones + " cones I only have " + allCones + "...");
+    }
+} while (allCones > 0);
 
-console.log("");
+console.log("Yay! I sold them all!");
 
-var allCones =  Math.floor(Math.random() * 50) + 50;
- do{
-     console.log("I sold " + + " cones." )
- }while()
