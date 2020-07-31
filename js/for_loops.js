30af70d25e38563d6021b81217303356ef31f910
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-07-30 19:46:38 -0500

    practiced for loops exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,38 +1,37 @@
 "use strict"
 
-function showMultiplicationTable(x) {
-    for (var y = 1; y <= 10; y++) {
-        console.log(x + " X " + y + " = "  + (x*y));
+function showMultiplicationTable(x){
+    for (var y = 1; y <= 10; y++){
+        console.log(x + " X " + y + " = " + (x*y));
     }
 }
 
-showMultiplicationTable(9);
+showMultiplicationTable(6);
 
 console.log("");
 
-function randomNumber(min,max) { 
-    return Math.floor(Math.random() * (min - max) + min); }
 
-for (var x = 1; x <= 10 ; x ++){
-    var randomGen = randomNumber(20,200)
-    if (randomGen % 2 === 0){
-        console.log(randomGen + " is even!")
-
+for (var y = 1; y <= 10; y++){
+    var randomNumber = Math.floor((Math.random()*200)+20);
+    if (randomNumber % 2 === 0){
+        console.log(randomNumber + " is even")
     } else{
-        console.log(randomGen + " is odd!")
+        console.log(randomNumber + " is odd")
     }
 }
 
 console.log("");
 
-
-for (var pyramid =1; pyramid <= 9; pyramid++){
+for (var pyramid = 1; pyramid <= 9; pyramid++ ){
     console.log(pyramid.toString().repeat(pyramid))
 }
- console.log("");
 
-for (var countdown = 100; countdown >= 5; countdown -=5){
+console.log("");
+
+for (var countdown = 100; countdown >= 5; countdown-=5){
     console.log(countdown);
 }
+
+
 
 
