83f6d1e66821d7691e24e3e8a30999d0506016f7
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-14 13:37:44 -0500

    retreived data/ creating template for data insertion

--- js/fers-JSON-warmup.js
+++ js/fers-JSON-warmup.js
@@ -1,23 +1,23 @@
-{
-    "name": "Bash",
-    "number_of_students": 2,
-    "location": "Dallas, TX",
-    "students": [
-        {
-            "name": "Stacy R",
-            "dob": "1990-01-24",
-            "scholarship": true,
-            "capstone": null
-        },
-        {
-            "name": "Juan M",
-            "dob": "1992-05-13",
-            "scholarship": false,
-            "capstone": null
-        }
-
-    ],
-    "remote": false,
-    "Instructors": "Fernando M, Douglas H"
-
-}
+// {
+//     "name": "Bash",
+//     "number_of_students": 2,
+//     "location": "Dallas, TX",
+//     "students": [
+//         {
+//             "name": "Stacy R",
+//             "dob": "1990-01-24",
+//             "scholarship": true,
+//             "capstone": null
+//         },
+//         {
+//             "name": "Juan M",
+//             "dob": "1992-05-13",
+//             "scholarship": false,
+//             "capstone": null
+//         }
+//
+//     ],
+//     "remote": false,
+//     "Instructors": "Fernando M, Douglas H"
+//
+// }
