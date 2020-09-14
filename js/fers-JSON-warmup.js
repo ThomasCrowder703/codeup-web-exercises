8a2bd4a244166dbf96c87b0cfe370edec06ed556
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-14 09:47:33 -0500

    converted PDF contents to JSON format

---
+++ js/fers-JSON-warmup.js
@@ -0,0 +1,23 @@
+var cohort ={
+    "name": "Bash",
+    "number_students": 2,
+    "location": "Dallas, TX",
+    "students": [
+        student1 = {
+            "name": "Stacy R",
+            "DOB": "1990-1-24",
+            "scholarship": true,
+            "capstone": null
+        },
+        student2 = {
+            "name": "Juan M",
+            "DOB": "1992-5-13",
+            "scholarship": false,
+            "capstone": null
+        }
+
+    ],
+    "remote": false,
+    "Instructors": "Fernando M, Douglas H"
+
+}
