7fe70e427ce7afea4bebaa167582efef9211605e
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-17 16:30:44 -0500

    Replaced object literal with string shorthand

--- js/es6.js
+++ js/es6.js
@@ -33,20 +33,22 @@ const users = [
 // to the languages array
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
-var name = 'your_name_here';
-var email = '';
-var languages = [];
+const name = 'Thomas';
+const email = 'thomas.levi.crowder3@gmail.com';
+const languages = ["html", "css", "javascript"];
 
 // TODO: rewrite the object literal using object property shorthand
 users.push({
-  name: name,
-  email: email,
-  languages: languages
+  name,
+  email,
+  languages
 });
 
+
+
 // TODO: replace `var` with `let` in the following variable declarations
-var emails = [];
-var names = [];
+let emails = [];
+let names = [];
 
 // TODO: rewrite the following using arrow functions
 users.forEach(function(user) {
