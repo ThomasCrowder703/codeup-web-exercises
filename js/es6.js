aa819e8d8a1cdf4684441e85d46630b8d46ad2c9
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-17 21:55:17 -0500

    working on template string portion

--- js/es6.js
+++ js/es6.js
@@ -71,7 +71,7 @@ users.forEach(function(user) {
 
 
   // TODO: rewrite the assignment below to use template strings
-  developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+  developers.push(${name} '\'s email is   ${email + name}  knows  + languages.join(', ')');
 });
 
 // TODO: Use `let` for the following variable
