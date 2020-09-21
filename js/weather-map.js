654731182f36610590dd102f058196f4291a47d2
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-20 21:10:39 -0500

    spaced out dates by day instead of by hour

--- js/weather-map.js
+++ js/weather-map.js
@@ -70,8 +70,21 @@
             "units": "imperial"
         }).done(function (data) {
 
-            console.log(data)
-            $("#date").empty().append(data.list[0].dt_txt);
+
+
+                let dateArray = []
+                for (let i = 0; i < data.list.length-1; i++){
+                    if(data.list[i + 1].dt_txt.charAt(9) === data.list[i].dt_txt.charAt(9)){
+                       continue;
+                    }else{
+                        dateArray.push(data.list[i].dt_txt);
+                        console.log(dateArray);
+                    }
+
+                }
+
+            // console.log(data.list)
+            $("#date").empty().append(dateArray[0]);
             $("#tempToday").empty().append(data.list[0].main.temp);
             $("#descriptionToday").empty().append("Description: " + data.list[0].weather[0].description);
             $("#humidToday").empty().append("Humidity: " + data.list[0].main.humidity + "%");
@@ -79,33 +92,34 @@
             $("#pressureToday").empty().append("Pressure: " + data.list[0].main.pressure)
 
 
-            $("#date2").empty().append(data.list[2].dt_txt);
+            $("#date2").empty().append(dateArray[1]);
             $("#temp2").empty().append(data.list[2].main.temp);
             $("#description2").empty().append("Description: " + data.list[2].weather[0].description);
             $("#humidity2").empty().append("Humidity:" + data.list[2].main.humidity);
             $("#wind2").empty().append("Wind: " + Math.floor(data.list[2].wind.speed));
             $("#pressure2").empty().append("Pressure: " + data.list[2].main.pressure);
 
-            $("#date3").empty().append(data.list[10].dt_txt);
+            $("#date3").empty().append(dateArray[2]);
             $("#temp3").empty().append(data.list[10].main.temp);
             $("#description3").empty().append("Description: " + data.list[10].weather[0].description);
             $("#humidity3").empty().append("Humidity:" + data.list[10].main.humidity);
             $("#wind3").empty().append("Wind: " + Math.floor(data.list[10].wind.speed));
             $("#pressure3").empty().append("Pressure: " + data.list[10].main.pressure);
 
-            $("#date4").empty().append(data.list[18].dt_txt);
+            $("#date4").empty().append(dateArray[3]);
             $("#temp4").empty().append(data.list[18].main.temp);
             $("#description4").empty().append("Description: " + data.list[18].weather[0].description);
             $("#humidity4").empty().append("Humidity:" + data.list[18].main.humidity);
             $("#wind4").empty().append("Wind: " + Math.floor(data.list[18].wind.speed));
             $("#pressure4").empty().append("Pressure: " + data.list[18].main.pressure);
 
-            $("#date5").empty().append(data.list[26].dt_txt);
+            $("#date5").empty().append(dateArray[4]);
             $("#temp5").empty().append(data.list[26].main.temp);
             $("#description5").empty().append("Description: " + data.list[26].weather[0].description);
             $("#humidity5").empty().append("Humidity:" + data.list[26].main.humidity);
             $("#wind5").empty().append("Wind: " + Math.floor(data.list[26].wind.speed));
             $("#pressure5").empty().append("Pressure: " + data.list[26].main.pressure);
+
 
         });
 
