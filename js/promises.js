ad039461e9182fbb5d08b5f602f31f19ba231516
Thomas Crowder <thomas.levi.crowder3@gmail.com>
2020-09-21 14:10:59 -0500

    added array with names but did not hook up to first function

--- js/promises.js
+++ js/promises.js
@@ -8,7 +8,7 @@ function getLastCommit(param) {
             response.json().then(users => {
                 for (var i =0; i <users.length; i++) {
                     if (users[i].type === "PushEvent") {
-                        console.log("Latest commit : " + users[i].created_at)
+                        console.log("Latest commit for " + param + " : " + users[i].created_at)
                         break;
                     }
                 }
@@ -18,5 +18,18 @@ function getLastCommit(param) {
 }
 
 
+// let loginName = []
+//     fetch("https://api.github.com/users", {headers: {'Authorization': 'token 6048dc904d8ae911b884437ba68a04f3df110f98'}})
+//         .then(response => {
+//             response.json().then(users => {
+//                 for( let i = 0; i < users.length; i++){
+//                     loginName.push(users[i].login)
+//                 }
+//
+//             })
+//         })
+// console.log(loginName)
 
 getLastCommit("ThomasCrowder703")
+
+
